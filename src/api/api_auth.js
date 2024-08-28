fefe0315d7ef5ec0ec6057e111d17785c4360a9b
Joojinsung <dev.jinsung1017@gmail.com>
2024-08-28 12:22:40 +0900

    token 추출

--- src/api/api_auth.js
+++ src/api/api_auth.js
@@ -1,13 +1,15 @@
 import axios from 'axios';
 import { API_URL } from '@/config/url';
 
-// 로그인 API 호출
 export const login = async (credentials) => {
     try {
         const response = await axios.post(`${API_URL}/api/user/login`, credentials);
 
         // 서버 응답 헤더에서 JWT 토큰 추출
         const token = response.headers['authorization'].split(' ')[1];  // 'Bearer <token>' 형식에서 <token> 추출
+
+        // 로컬 스토리지에 토큰 저장
+        localStorage.setItem('token', token);
 
         return { token };  // 추출한 토큰을 반환
     } catch (e) {
