50ecc49b66d4ea7634f67fb1c560b6593c949a6b
Joojinsung <dev.jinsung1017@gmail.com>
2024-08-28 12:19:32 +0900

    token 추출

--- src/api/api_auth.js
+++ src/api/api_auth.js
@@ -7,7 +7,7 @@ export const login = async (credentials) => {
         const response = await axios.post(`${API_URL}/api/user/login`, credentials);
 
         // 서버 응답 헤더에서 JWT 토큰 추출
-        const token = response.headers['authorization'].split(' ')[1];  // 'Bearer <token>' 형식에서 <token> 추출
+        const token = response.headers['Authorization'].split(' ')[1];  // 'Bearer <token>' 형식에서 <token> 추출
         const user = response.data.user;  // 응답 데이터에서 사용자 정보 추출
 
         return { token, user };  // 추출한 토큰과 사용자 정보를 반환
